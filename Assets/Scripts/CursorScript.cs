20266c3c51ae022e3cd1b0111f8f6e930e272b24
joelponson <ponson.joel@gmail.com>
2017-06-22 11:58:48 +0200

    enable multiplayer on online serve. Disable cursor in game

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+    private bool cursorHide;
+
+    // Use this for initialization
+    void Start () {
+        cursorHide = true;
+        UpdateCursor();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            cursorHide = false; UpdateCursor();
+        } else if (Input.GetMouseButtonDown(0))
+        {
+            cursorHide = true;
+            UpdateCursor();
+        }
+    }
+
+    void UpdateCursor()
+    {
+        if (cursorHide)
+        {
+            Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.None; Cursor.visible = true;
+        }
+    }
+}
